d0f8769d5e963473de6b78d35e34ae009a2c3c66
Yimspc <S6401072510338@email.kmutnb.ac.th>
2023-10-21 04:17:21 +0700

    CV

---
+++ Resume.js
@@ -0,0 +1,23 @@
+function greet() {
+    alert("Welcom in Resume");
+}
+
+function thip() {
+    var image = document.getElementById("hiddenthip");
+    if (image.style.display === "none") {
+        image.style.display = "block"; // เมื่อคลิกที่ปุ่ม แสดงรูปภาพ
+    } else {
+        image.style.display = "none"; // เมื่อคลิกอีกครั้ง ซ่อนรูปภาพ
+    }
+}
+
+function siem() {
+    var image = document.getElementById("hiddensiem");
+    if (image.style.display === "none") {
+        image.style.display = "block"; // เมื่อคลิกที่ปุ่ม แสดงรูปภาพ
+    } else {
+        image.style.display = "none"; // เมื่อคลิกอีกครั้ง ซ่อนรูปภาพ
+    }
+}
+
+
